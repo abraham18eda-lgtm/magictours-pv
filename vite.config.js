62911b9ef167aec6c31305065bcc1dc15e691a02
proyect2024-admin <abraham18eda@gmail.com>
2026-03-23 23:26:31 -0600

    se corrigen archivos para correr en vercel

--- vite.config.js
+++ vite.config.js
@@ -3,24 +3,29 @@ import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
 import path from 'path';
 
+
+const isVercel = process.env.VERCEL === '1'
+
 export default defineConfig({
   plugins: [
     vue(),
-    laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'],
-      refresh: true,
-      // Aquí le decimos al plugin exactamente dónde poner el manifest
-      manifest: 'public/build/manifest.json',
-      buildDirectory: 'build', // carpeta donde irán los assets
-    }),
-  ],base: '/',
+    !isVercel &&
+      laravel({
+        input: ['resources/css/app.css', 'resources/js/app.js'],
+        refresh: true,
+        // Aquí le decimos al plugin exactamente dónde poner el manifest
+        manifest: 'public/build/manifest.json',
+        buildDirectory: 'build', // carpeta donde irán los assets
+      }),
+  ],
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'resources/js'),
     },
   },
   build: {
-    outDir: 'public/build', // carpeta final de los assets
+    outDir: isVercel ? 'dist' : 'public/build',
     emptyOutDir: true,
     rollupOptions: {
       input: {
